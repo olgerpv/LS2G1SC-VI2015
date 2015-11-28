3399b17c63cbe77e85358c1b48f14f7dcf832c85
Olger Parra <critsol.sa@gmail.com>
2015-11-28 15:59:42 -0600

    Segundo script Saluda.sh

---
+++ Scripts/LAB03/saluda.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -x
+if [ $# == 0 ]
+then
+	echo "debe de usar un parametro posicional.."
+else
+	if [ $1 == "Olger" ]
+	then
+		echo "Hola Olger"
+	else
+		echo "No se quien eres"
+		read nombre
+		echo "Hola $nombre"
+	fi
+fi
