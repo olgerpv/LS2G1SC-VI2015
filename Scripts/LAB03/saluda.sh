02239b5122b6aae9b5b330e13c4977125025fa9d
Olger Parra <critsol.sa@gmail.com>
2015-11-28 16:30:54 -0600

    NO CASE SENTIVE

--- Scripts/LAB03/saluda.sh
+++ Scripts/LAB03/saluda.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+
 if [ $# == 0 ]
 then
 	echo "debe de usar un parametro posicional.."
 else
-	if [ $1 == "Olger" ]
+	nombre="$1"
+	nombre_limpio=$(echo $nombre | tr "[:upper:]" "[:lower:]")
+
+	if [ $nombre_limpio  == "olger" ]
 	then
-		echo "Hola Olger"
+		echo "Hola estudianteOlger"
 	elif [ $1 == "Nadie" ]
 	then
 		echo "No puedo saludar a nadie.."
